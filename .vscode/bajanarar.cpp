6981159d8a6fc9234976f6f54d170f027e88a4dd
Gor Tamrazyan <gor.tamrazyan2002gmail.com>
2024-09-16 13:36:50 +0400

    first commit

---
+++ .vscode/bajanarar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+int
+main()
+{
+    int a = 0, b = 0;
+    std::cin >> a;
+    std::cin >> b;
+    int s;
+    if (a < b) {
+        for (int i = 0; i < a; i++) {
+            
+        }
+    } else {
+
+    }
+
+    return 0;
+}
